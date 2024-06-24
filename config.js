ad8441b34f5efcf455a65a9ee22875a4d6962fa9
MCg <minhcongnguyen11@gmail.com>
2024-06-24 20:59:37 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "My Lover",
+
+    background: "background.jpg",
+
+    min1: "1.jpg",
+    min2: "2.jpg",
+    min3: "3.jpg",
+    min4: "4.jpg",
+    min5: "5.jpg",
+    min6: "6.jpg",
+
+    max1: "7.jpg",
+    max2: "8.jpg",
+    max3: "1.jpg",
+    max4: "2.jpg",
+    max5: "3.jpg",
+    max6: "4.jpg",
+}
